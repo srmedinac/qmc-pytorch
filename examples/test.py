cf2b244cdf315937236ee98b32fad45e7c2dfc88
Sebastian Medina <srmedinac@unal.edu.co>
2022-05-27 18:45:10 -0500

    package fix

--- examples/test.py
+++ examples/test.py
@@ -2,10 +2,13 @@
 Tests
 """
 import sys
-sys.path.insert(0, "../")
 import torch
-import torch.nn as nn
+import numpy as np
 from qmc.torch import layers
 from qmc.torch import models
 
+data_x = torch.tensor([[1,2],[0,1],[3,1],[0,2]])
+fm_x = layers.QFeatureMapOneHot(4)
 
+#print(fm_x)
+print(torch.functional.one_hot(data_x, 4))
--- qmc/torch/models.py
+++ qmc/torch/models.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from . import layers
+import layers
 
 class QMClassifier(nn.Module):
     """
