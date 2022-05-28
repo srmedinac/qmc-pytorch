0a81e19bf5f68a2d1c2b8a791ffab696db8b7863
Sebastian Medina <srmedinac@unal.edu.co>
2022-05-27 22:32:19 -0500

    test update

--- qmc/test.py
+++ qmc/test.py
@@ -3,12 +3,22 @@ Tests
 """
 import sys
 import torch
+import unittest
 import numpy as np
 import pt.layers as layers
 import pt.models as models
 
-data_x = torch.tensor([[1,2],[0,1],[3,1],[0,2]])
-fm_x = layers.QFeatureMapOneHot(4)
 
-print(fm_x(data_x))
-print(torch.nn.functional.one_hot(data_x, 4))
+class TestQFeatureMapOneHot(unittest.TestCase):
+    def test_one_hot(self):
+        self.data_x = torch.tensor([[1, 2], [0, 1], [3, 1], [0, 2]])
+        self.fm_x = layers.QFeatureMapOneHot(4)
+        self.assertEqual(self.fm_x(self.data_x).shape, (4, 16), "Wrong output shape")
+        self.assertTrue(torch.equal(self.fm_x(self.data_x), torch.tensor([[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                               [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                                               [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])),"FAILED: QFeatureMapOneHot")
+
+
+if __name__ == '__main__':
+    unittest.main()
